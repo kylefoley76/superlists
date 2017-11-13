b2481193a547f9b43df6be5b360a548af747b23d
kylefoley <kylefoley202@hotmail.com>
2017-11-12 21:51:51 -0800

    hey

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,44 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from lists.models import Item
+
+# def home_page(request):
+#     if request.method == 'POST':
+#         return HttpResponse(request.POST['item_text'])
+#     return render(request, 'home.html')
 
 
 
-
+# def home_page(request):
+#     if request.method == 'POST':
+#         Item.objects.create(text=request.POST['item_text'])
+#         return redirect('/')
+#
+#     return render(request, 'home.html')
 
 def home_page(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+#
+# def home_page(request):
+#     # item = Item()
+#     # item.text = request.POST.get('item_text', '')
+#     # item.save()
+#
+#     if request.method == 'POST':
+#         new_item_text = request.POST['item_text']
+#         Item.objects.create(text=new_item_text)
+#     else:
+#         new_item_text = ''
+#
+#     return render(request, 'home.html', {
+#         'new_item_text': new_item_text,
+#     })
+
+
