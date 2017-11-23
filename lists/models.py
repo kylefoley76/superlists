dc24e9fd836cfcfa81477c77e57fb7b9e52b8ce6
kylefoley <kylefoley202@hotmail.com>
2017-11-23 09:16:37 -0800

    hey

--- lists/models.py
+++ lists/models.py
@@ -2,5 +2,17 @@ from django.db import models
 
 
 
+
+
+class List(models.Model):
+    pass
+
+
+
+# class Item(models.Model):
+#     text = models.TextField(default='')
+#     list = models.TextField(default='')
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
--- lists/views.py
+++ lists/views.py
@@ -1,44 +1,28 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
-# def home_page(request):
-#     if request.method == 'POST':
-#         return HttpResponse(request.POST['item_text'])
-#     return render(request, 'home.html')
-
-
-
-# def home_page(request):
-#     if request.method == 'POST':
-#         Item.objects.create(text=request.POST['item_text'])
-#         return redirect('/')
 #
-#     return render(request, 'home.html')
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
+
+
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
-#
-# def home_page(request):
-#     # item = Item()
-#     # item.text = request.POST.get('item_text', '')
-#     # item.save()
-#
-#     if request.method == 'POST':
-#         new_item_text = request.POST['item_text']
-#         Item.objects.create(text=new_item_text)
-#     else:
-#         new_item_text = ''
-#
-#     return render(request, 'home.html', {
-#         'new_item_text': new_item_text,
-#     })
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
 
 
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
