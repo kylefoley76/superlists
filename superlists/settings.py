caba2e04b0cec8d5ade863b37ea055e7c3b2ebfc
kylefoley <kylefoley202@hotmail.com>
2017-12-02 03:06:07 -0800

    adding site in allowed hosts

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2d-%3ci(*@1aa#m-0rab!n3mz9s%t6uvhf$)l5hxw=y4ss+wmj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kylefoley.club']
 
 
 # Application definition
@@ -103,4 +103,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
